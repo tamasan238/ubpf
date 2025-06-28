9bf3e41fab8655f24b27b41c3b5901c19de2fe15
masaki <m1117@aol.jp>
2025-06-28 23:18:48 +0900

    try

--- vm/test.c
+++ vm/test.c
@@ -280,7 +280,7 @@ receive_packets(ubpf_jit_fn fn)
                 (packets*SHM_SIZE_PER_PACKET), dp_packet2_size);
 
             // packet
-            packet == NULL;
+            packet = NULL;
             if(dp_packet2->allocated_ == 0){
                 printf("allocated_ is 0\n\n");
                 // fn_ret=3;
