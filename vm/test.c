6b0ced083dbd0da587184242c8c9aac778629387
masaki <m1117@aol.jp>
2025-07-18 02:48:17 +0900

    try

--- vm/test.c
+++ vm/test.c
@@ -73,7 +73,8 @@ typedef struct
     int p4runtime_id;
 } Connection;
 
-#define MAX_CONNECTIONS 512
+// #define MAX_CONNECTIONS 512
+#define MAX_CONNECTIONS 8
 #define SHM_SESSION_TABLE META_AREA
 #define SHM_TABLE_IS_LOCKED (SHM_SESSION_TABLE + sizeof(Connection) * MAX_CONNECTIONS)
 
@@ -358,7 +359,7 @@ receive_packets(ubpf_jit_fn fn)
             if(ovs_tid == -1)
             {
                 // not linked with ovs thread
-                usleep(10); // 10us
+                usleep(1000); // 1ms
                 continue;
             }
             offset = calc_offset(session_id);
