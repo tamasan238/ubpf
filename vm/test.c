fb4111ff3513ef8c494fa5d104c6490378610f40
Masaki Iwai <m1117@aol.jp>
2025-11-07 23:26:29 +0900

    update

--- vm/test.c
+++ vm/test.c
@@ -73,7 +73,7 @@ typedef struct
     int p4runtime_id;
 } Connection;
 
-#define MAX_CONNECTIONS 512
+#define MAX_CONNECTIONS 32
 // #define MAX_CONNECTIONS 8
 #define SHM_SESSION_TABLE META_AREA
 #define SHM_TABLE_IS_LOCKED (SHM_SESSION_TABLE + sizeof(Connection) * MAX_CONNECTIONS)
