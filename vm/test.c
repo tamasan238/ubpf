2c84c371bd8546d491b26de9f25608a9ff144989
Masaki I. <m1117@aol.jp>
2025-11-13 20:13:20 +0900

    update

--- vm/test.c
+++ vm/test.c
@@ -320,9 +320,6 @@ shm_start(void)
     }
 
     syslog(LOG_WARNING, "SHM opened. mapped to %p", shm_ptr);
-
-    syslog(LOG_WARNING, "TESTTESTTESTTESTTESTTEST");
-    syslog(LOG_INFO, "TESTTESTTESTTESTTESTTESTTEST");
 }
 
 void
